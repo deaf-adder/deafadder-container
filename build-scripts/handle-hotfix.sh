a85b6f1e35784b8fa5c8d0ca80811f5c3313480f
Leddzip <leddzip@gmail.com>
2021-11-27 18:45:31 +0100

    feature/DEAFADDER-5_integrate-qodana-for-cricleci-for-deafadder-container
    qodana artifact done, now trying to get test results in circle ci

--- build-scripts/handle-hotfix.sh
+++ build-scripts/handle-hotfix.sh
@@ -21,7 +21,9 @@ function incremented_bugfix() {
 function update-version-everywhere() {
   local new_version="$1"
   ./build-scripts/update-version.sh "$new_version"
-  git add --all
+  git add deafadder_container/__init__.py
+  git add tests/test_deafadder_container.py
+  git add pyproject.toml
   git commit -m "update bugfix version to $new_version"
   git push
 }
